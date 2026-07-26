1dcd11631160822110a75c6e1be154a2ad751641
devmuzammil <muzammilhussainabro@gmail.com>
2026-07-26 16:05:00 +0500

    feat(routes): add Zod validation to notes routes

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const protect = require('../middleware/authMiddleware');
+const validateRequest = require('../middleware/validateRequest');
+const { createNoteSchema, updateNoteSchema, paramsIdSchema } = require('../utils/validationSchemas');
 const {
   createNoteController,
   getNotesController,
@@ -11,10 +13,10 @@ const {
 const router = express.Router();
 
 router.use(protect);
-router.post('/', createNoteController);
+router.post('/', validateRequest(createNoteSchema), createNoteController);
 router.get('/', getNotesController);
-router.get('/:id', getNoteByIdController);
-router.put('/:id', updateNoteController);
-router.delete('/:id', deleteNoteController);
+router.get('/:id', validateRequest(paramsIdSchema), getNoteByIdController);
+router.put('/:id', validateRequest(paramsIdSchema), validateRequest(updateNoteSchema), updateNoteController);
+router.delete('/:id', validateRequest(paramsIdSchema), deleteNoteController);
 
 module.exports = router;
